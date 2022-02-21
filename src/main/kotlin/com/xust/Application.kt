7fb677f1e01903306a618d25befa6e45d5918f65
Sulemanorg <1916769125@qq.com>
2022-02-21 11:59:46 +0800

    fileName: build.gradle, Application.kt reason: Add https support

--- src/main/kotlin/com/xust/Application.kt
+++ src/main/kotlin/com/xust/Application.kt
@@ -3,9 +3,42 @@ package com.xust
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.xust.plugins.*
+import io.ktor.application.*
+import io.ktor.network.tls.certificates.*
+import org.slf4j.LoggerFactory
+import java.io.File
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
+
+    /*embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
         configuration()
-    }.start(wait = true)
+    }.start(wait = true)*/
+
+    val keyStoreFile = File("build/keystore.jks")
+    val keystore = generateCertificate(
+        file = keyStoreFile,
+        keyAlias = "sampleAlias",
+        keyPassword = "foobar",
+        jksPassword = "foobar"
+    )
+
+    val environment = applicationEngineEnvironment {
+        log = LoggerFactory.getLogger("ktor.application")
+        connector {
+            host = "127.0.0.1"
+            port = 8080
+        }
+        sslConnector(
+            keyStore = keystore,
+            keyAlias = "sampleAlias",
+            keyStorePassword = { "foobar".toCharArray() },
+            privateKeyPassword = { "foobar".toCharArray() }) {
+            host = "127.0.0.1"
+            port = 8443
+            keyStorePath = keyStoreFile
+        }
+        module(Application::configuration)
+    }
+
+    embeddedServer(Netty, environment).start(wait = true)
 }
